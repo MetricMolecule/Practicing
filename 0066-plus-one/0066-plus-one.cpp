22cda29889143649d8b1e37f7626038fcc716012
MetricMolecule <142025919+MetricMolecule@users.noreply.github.com>
2025-09-26 23:13:25 +0530

    Time: 0 ms (100%), Space: 11.4 MB (72.46%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n= digits.size();
+        int tot=0;
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
