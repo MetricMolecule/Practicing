2b63db3976ede362fe2aae7d7d05f77e38fe7933
Anshak <142025919+MetricMolecule@users.noreply.github.com>
2026-05-03 23:44:27 +0530

    Time: 38 ms (73.99%), Space: 45.4 MB (55.09%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> hashh;
+        hashh[0]=1;
+        int sum=0;
+        int res=0;
+        int x=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            x=sum-k;
+            if(hashh.find(x)!=hashh.end()){
+                res=res+hashh[x];
+            }
+            hashh[sum]++;
+        }
+        return res;
+    }
+};
