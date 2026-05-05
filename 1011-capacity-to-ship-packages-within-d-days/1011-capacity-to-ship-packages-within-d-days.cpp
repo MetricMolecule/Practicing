005fdbc14cddbb19bd17ee7ec02a103dfb3239fa
Anshak <142025919+MetricMolecule@users.noreply.github.com>
2026-05-05 21:42:26 +0530

    Time: 3 ms (99.25%), Space: 35 MB (52.72%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-        int low =*max_element(weights.begin(), weights.end());
-        int high=0;
-        for(int i : weights){
-            high+= i;
+        int total=0;
+        int maxi=0;
+        for(int w : weights){
+            total+=w;
+            maxi=max(maxi,w);
         }
-        while(low<high){
-            int mid=(low+high)/2;
+        int low= maxi;
+        int high= total;
+        while(low<=high){
+            int mid= low+(high-low)/2;
+            int x=1;
             int check=0;
-            int ch=1;
-            for(int i : weights){
-                check+= i;
-                if(check > mid){
-                    ch++;
+            for(int i :weights){
+                check+=i;
+                if(check>mid){
+                    x++;
                     check=i;
                 }
-            }if(ch<=days){high=mid;}
-            else{low=mid+1;}
+            }
+            if(x<=days){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
         }return low;
     }
 };
