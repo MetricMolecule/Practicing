4ccc97fe7a8868333fcc647ea91e5e9aa476280a
Anshak <142025919+MetricMolecule@users.noreply.github.com>
2026-05-05 17:39:04 +0530

    Time: 82 ms (55.22%), Space: 88.8 MB (67.65%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -5,9 +5,11 @@ public:
         int longest=0;
         unordered_set<int> st(nums.begin(),nums.end());
         for(int n : st){
-            if(st.find(n-1)==st.end()){
+            // finding the element which doesnt have its predecessor, start of sequence
+            if(st.find(n-1)==st.end()){ 
                 int curr= n;                
                 int len=1;
+                // now adding the value on len variable till we keep on seeing successive numbers, as it is a set
                 while(st.find(curr+1)!=st.end()){
                     len+=1;
                     curr+=1;
