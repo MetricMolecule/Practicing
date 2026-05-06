87505324153ef7d183a8cbfb01b028c1e45f45de
Anshak <142025919+MetricMolecule@users.noreply.github.com>
2026-05-06 15:41:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
+    vector<vector<int>> ans; //global vector to store answer after recurstion
     void solve(vector<int>& nums,vector<int> temp, int i){
         int n=nums.size();
         if(i==n){
-            ans.push_back(temp);
-            return;
+            ans.push_back(temp);  //copying temp vector in ans vector, temp vector reused each time
+            return;    // ending condition of recursion fucntion(0-->size of array)
         }
+        solve(nums,temp,i+1); 
+        temp.push_back(nums[i]); 
         solve(nums,temp,i+1);
-        temp.push_back(nums[i]);
-        solve(nums,temp,i+1);
-        temp.pop_back();
+        temp.pop_back(); //backtracking, removing
     }
     
     vector<vector<int>> subsets(vector<int>& nums) {
