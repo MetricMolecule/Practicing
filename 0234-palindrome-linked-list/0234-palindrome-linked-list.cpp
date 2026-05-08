ba3a44f0950565659d5c4b9c0b879ee9e8bf327e
Anshak <142025919+MetricMolecule@users.noreply.github.com>
2026-05-08 22:00:07 +0530

    Time: 0 ms (100%), Space: 117.9 MB (81.12%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -11,17 +11,36 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        stack<int> st;
-        ListNode* temp= head;
-        while(temp!=nullptr){
-            st.push(temp->val);
-            temp=temp->next;
+        if(head==nullptr || head->next==nullptr) return true;
+        
+        //finding the middle
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            fast=fast->next->next;
+            slow=slow->next;
         }
-        temp = head;
-        while(temp!=nullptr){
-            if(temp->val!=st.top()) return false;
-            else st.pop();
-            temp=temp->next;
+        //reversing the other half
+        if(fast!=nullptr){
+            slow=slow->next;             //for odd length we dont need the middle one, so skip
+        }
+        ListNode* curr=slow;
+        ListNode* prev=nullptr;
+        while(curr!=nullptr){
+            ListNode* nextt=curr->next;  //setting up the next node which has value of curr's next
+            curr->next=prev;             //putting curr'next as previous
+            prev=curr;                   //moving prev ahead to curr
+            curr=nextt;                  // moving curr to its next (not directly)
+        }
+
+        ListNode* head1=head;
+        ListNode* head2=prev;
+        
+        while(head2!=nullptr){
+            if(head1->val!=head2->val) return false;
+            
+            head1=head1->next;
+            head2=head2->next;
         }return true;
     }
 };
