a3ed7385b081328804f47eeb13979feabdd3860b
Anshak <142025919+MetricMolecule@users.noreply.github.com>
2026-05-09 14:23:52 +0530

    Time: 3 ms (9.83%), Space: 9 MB (37.77%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
-                st.push(s[i]);
-            }else{
-                if(s.empty()) return false;
-                else if(s[i]==')' && st.top()!='(') return false;
-                else if(s[i]=='}' && st.top()!='{') return false;
-                else if(s[i]==']' && st.top()!='[') return false;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[') st.push(s[i]);
+            else{
+                if(st.empty()) return false;
+                if(s[i]==')' && st.top()!='(') return false;
+                if(s[i]=='}' && st.top()!='{') return false;
+                if(s[i]==']' && st.top()!='[') return false;
+
                 st.pop();
             }
-        }return st.empty();
+        }
+        return st.empty();
     }
 };
