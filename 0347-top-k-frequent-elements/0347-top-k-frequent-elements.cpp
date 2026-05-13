5492168673a7d4dda56e22040df0b539a6786a48
Anshak <142025919+MetricMolecule@users.noreply.github.com>
2026-05-13 18:47:24 +0530

    Time: 2 ms (57.17%), Space: 18 MB (38.75%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        //frequency storing hashmap
         unordered_map<int, int> freq;
         for(int i :nums){
             freq[i]++;
         }
+        //max heap to store with respect to frquencies as a pair, frequency first one of pair
         priority_queue<pair<int,int>> pq;
         for(auto it : freq){
             pq.push({it.second,it.first});
