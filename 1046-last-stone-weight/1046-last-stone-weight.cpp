ee066b53008fc2d767b33ce3e97f926fb726c404
Anshak <142025919+MetricMolecule@users.noreply.github.com>
2026-05-13 19:04:23 +0530

    Time: 0 ms (100%), Space: 9.8 MB (78.97%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq; //max heap to store and change on it
+        //initial store in priority queue;
+        for(int i:stones){
+            pq.push(i);
+        }
+        while(pq.size()>1){
+            int l1=pq.top();
+            pq.pop();
+            int l2=abs(pq.top()-l1);
+            pq.pop();
+            pq.push(l2);
+        }
+        return pq.top();
+    }
+};
