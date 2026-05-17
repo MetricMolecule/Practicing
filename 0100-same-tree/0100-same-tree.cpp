4aa280fe9dcfab0cc7a20b78162f2c5afde4637e
Anshak <142025919+MetricMolecule@users.noreply.github.com>
2026-05-17 21:45:39 +0530

    Time: 0 ms (100%), Space: 12.7 MB (76.61%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -15,8 +15,8 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p==NULL && q==NULL) return true;
         if(p==NULL || q==NULL) return false;
-        if(p->val != q->val) return false;
-        
+        if(p->val != q->val) return false; //forgot to match the values
+
         bool left=isSameTree(p->left,q->left);
         bool right=isSameTree(p->right,q->right);
         return left & right;    
