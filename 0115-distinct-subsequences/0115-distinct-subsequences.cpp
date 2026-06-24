be992c02176188e681cc9ac4d955c3a8d637a1b6
Anshak <142025919+MetricMolecule@users.noreply.github.com>
2026-06-24 19:23:02 +0530

    Time: 31 ms (52.12%), Space: 26.9 MB (71.36%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int solve(int i, int j, string& s, string& t, vector<vector<int>>& dp){
+        if(j<0) return 1;
+        if(i<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]==t[j]){
+            // if it matches, we pick + notpick, because there can be other ways to make the t overall
+            return dp[i][j]=solve(i-1,j-1,s,t,dp) + solve(i-1,j,s,t,dp);
+        }
+        else return dp[i][j]=solve(i-1,j,s,t,dp);
+    }
+    int numDistinct(string s, string t) {
+        int m=s.size();
+        int n=t.size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,s,t,dp);
+    }
+};
