bc39245791cace68ac57a3928061c16b4d303f71
Anshak <142025919+MetricMolecule@users.noreply.github.com>
2026-06-30 13:25:29 +0530

    Time: 30 ms (20.16%), Space: 11.2 MB (21.13%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     int numberOfSubstrings(string s) {
         unordered_map<char,int> mpp;
-        int i=0;
-        int j=0;
-        int n=s.length();
-        int count=0;
         mpp['a']=0;
         mpp['b']=0;
         mpp['c']=0;
-
-        while(j<n){
+        int ans=0;
+        int i=0;
+        int j=0;
+        while(j<s.size()){
             mpp[s[j]]++;
             while(mpp['a']>=1 && mpp['b']>=1 && mpp['c']>=1){
-               count += n-j; // made mistake here- at point j, every window right of it is also valid
-               mpp[s[i]]--;
-               i++;
+                ans+=s.size()-j;
+                mpp[s[i]]--;
+                i++;
             }
             j++;
         }
-        return count;
+        return ans;
     }
 };
