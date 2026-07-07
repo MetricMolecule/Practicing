56039f855697eb0d98ef1cd66eac3752ea2c1a7f
Anshak <142025919+MetricMolecule@users.noreply.github.com>
2026-07-07 10:07:49 +0530

    Time: 25 ms (89.37%), Space: 71.6 MB (57.42%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        pair<int,vector<int>> p;
-        priority_queue<
-            pair<int,vector<int>>,
-            vector<pair<int,vector<int>>>,
-            greater<pair<int,vector<int>>>
-            > pq;
-        for(int i=0;i<points.size();i++){
-            int x = points[i][0];
-            int y = points[i][1];
-            int dist = x*x + y*y ;
-            p={dist,points[i]};
-            pq.push(p);
+        // approach- use priority queue(min heap) to get answers in distance order
+        // pair- 1st value, is distance & second value is index of the point in points 2D vector
+        priority_queue<pair<int,int>,
+                       vector<pair<int,int>>,
+                       greater<pair<int,int>> > pq;
+        int n=points.size();
+        for(int i=0;i<n;i++){
+            int u=points[i][0];
+            int v=points[i][1];
+            int dis = (u*u) + (v*v);
+            pq.push({dis,i});
         }
         vector<vector<int>> ans;
+
         for(int i=0;i<k;i++){
-            ans.push_back(pq.top().second);
+            vector<int> temp;
+            auto p = pq.top();
             pq.pop();
+            int index=p.second;
+            temp=points[index];
+            ans.push_back(temp);
         }
         return ans;
+
     }
 };
